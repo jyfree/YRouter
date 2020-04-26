1e17b4e6b0b71740ea13362c3b559c5ccb95f7f1
jy <123456>
2020-04-26 18:45:19 +0800

    修改示例

--- simple-kotlin/src/main/java/com/jy/yrouter/simple/kotlin/KotlinActivity.kt
+++ simple-kotlin/src/main/java/com/jy/yrouter/simple/kotlin/KotlinActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.jy.yrouter.Router
 import com.jy.yrouter.annotation.RouterUri
 import com.jy.yrouter.appbaselibrary.DemoConstant
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_kotlin.*
 
 @RouterUri(path = [DemoConstant.KOTLIN])
-class MainActivity : AppCompatActivity() {
+class KotlinActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_kotlin)
 
         val a = Router.callMethod(DemoConstant.ADD_METHOD, 2, 3) as Int
         tv.text = "Kotlin模块调用Java模块的Service \n 2 + 3 = {$a}"
--- simple-kotlin/src/main/java/debug/DemoApplication.java
+++ simple-kotlin/src/main/java/debug/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.jy.yrouter.simple.kotlin;
+package debug;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
